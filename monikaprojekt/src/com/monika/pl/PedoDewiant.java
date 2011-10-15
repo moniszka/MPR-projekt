7ae4aa4515b2fd48b2dd6eddda944721d5c5814d
Monika <Monika@89.72.197.232>
2011-10-15 20:40:24 +0200

    klasa PedoDewiant

---
+++ monikaprojekt/src/com/monika/pl/PedoDewiant.java
@@ -0,0 +1,13 @@
+package com.monika.pl;
+
+public class PedoDewiant {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("abc");
+	}
+
+}
